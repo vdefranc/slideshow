80f31a364dcaf75170de7ab1877485bf535541fd
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-02-12 14:28:59 -0500

    indicator function also working

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,33 +1,44 @@
-var CHESLIDESHOW = {
-	currentSlide: 0,
-	slideSelector: document.getElementsByClassName('che-slideshow-slide'),
-	indicatorSelector: document.getElementsByClassName('slideshow-indicator'),
-	nextSlide: function () {
-		var nextSlide = this.slideSelector[this.currentSlide + 1] === undefined ?
-			0 :  this.currentSlide + 1;
+var CHESLIDESHOW = (function () {
+	var currentSlide = 0;
+	var slideSelector = document.getElementsByClassName('che-slideshow-slide');
+	var indicatorSelector = document.getElementsByClassName('slideshow-indicator');
 
-		this.slideSelector[this.currentSlide].classList.add("inactive");
-		this.indicatorSelector[this.currentSlide].classList.add("inactive-indicator");
-	
-		this.slideSelector[nextSlide].classList.remove("inactive");
-		this.indicatorSelector[nextSlide].classList.remove("inactive-indicator");
+	return {
+		nextSlide: function () {
+			var nextSlide = slideSelector[currentSlide + 1] === undefined ?
+				0 :  currentSlide + 1;
 
-		this.currentSlide = nextSlide;
-	},
-	prevSlide: function () {
+			slideSelector[currentSlide].classList.add("inactive");
+			indicatorSelector[currentSlide].classList.add("inactive-indicator");
+		
+			slideSelector[nextSlide].classList.remove("inactive");
+			indicatorSelector[nextSlide].classList.remove("inactive-indicator");
 
-		var prevSlide = this.slideSelector[this.currentSlide - 1] === undefined ?
-			this.slideSelector.length - 1 :  this.currentSlide - 1;
+			currentSlide = nextSlide;
+		},
+		prevSlide: function () {
 
-		this.slideSelector[this.currentSlide].classList.add("inactive");
-		this.indicatorSelector[this.currentSlide].classList.add("inactive-indicator");
-	
-		this.slideSelector[prevSlide].classList.remove("inactive");
-		this.indicatorSelector[prevSlide].classList.remove("inactive-indicator");
+			var prevSlide = slideSelector[currentSlide - 1] === undefined ?
+				slideSelector.length - 1 :  currentSlide - 1;
 
-		this.currentSlide = prevSlide;
-	},
-	selectSlide: function (slide) {
-		this.currentSlide = slide;
-	}
-};
+			slideSelector[currentSlide].classList.add("inactive");
+			indicatorSelector[currentSlide].classList.add("inactive-indicator");
+		
+			slideSelector[prevSlide].classList.remove("inactive");
+			indicatorSelector[prevSlide].classList.remove("inactive-indicator");
+
+			currentSlide = prevSlide;
+		},
+		selectSlide: function (slide) {
+
+			slideSelector[currentSlide].classList.add("inactive");
+			indicatorSelector[currentSlide].classList.add("inactive-indicator");
+		
+			slideSelector[slide].classList.remove("inactive");
+			indicatorSelector[slide].classList.remove("inactive-indicator");
+
+
+			currentSlide = slide;
+		}
+	};
+})();

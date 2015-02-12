c21d8159cc710167e92665a63ea3338122fe2b19
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-02-12 14:20:38 -0500

    functioning without animations

--- dist/slideshow.js
+++ dist/slideshow.js
@@ -1,1 +1,33 @@
-var foo = 3;
+var CHESLIDESHOW = {
+	currentSlide: 0,
+	slideSelector: document.getElementsByClassName('che-slideshow-slide'),
+	indicatorSelector: document.getElementsByClassName('slideshow-indicator'),
+	nextSlide: function () {
+		var nextSlide = this.slideSelector[this.currentSlide + 1] === undefined ?
+			0 :  this.currentSlide + 1;
+
+		this.slideSelector[this.currentSlide].classList.add("inactive");
+		this.indicatorSelector[this.currentSlide].classList.add("inactive-indicator");
+	
+		this.slideSelector[nextSlide].classList.remove("inactive");
+		this.indicatorSelector[nextSlide].classList.remove("inactive-indicator");
+
+		this.currentSlide = nextSlide;
+	},
+	prevSlide: function () {
+
+		var prevSlide = this.slideSelector[this.currentSlide - 1] === undefined ?
+			this.slideSelector.length - 1 :  this.currentSlide - 1;
+
+		this.slideSelector[this.currentSlide].classList.add("inactive");
+		this.indicatorSelector[this.currentSlide].classList.add("inactive-indicator");
+	
+		this.slideSelector[prevSlide].classList.remove("inactive");
+		this.indicatorSelector[prevSlide].classList.remove("inactive-indicator");
+
+		this.currentSlide = prevSlide;
+	},
+	selectSlide: function (slide) {
+		this.currentSlide = slide;
+	}
+};
--- src/js/tests/slideshow-specs.js
+++
@@ -1,7 +0,0 @@
-/*
-describe('foo', function () {
-	it('equals 3', function () {
-		expect(foo).toEqual(3);
-	});
-});
-*/

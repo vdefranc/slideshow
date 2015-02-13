2e414039b5a47eeec302d860688c5342f8cfd607
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-02-13 13:56:13 -0500

    prev slide working. next slide still wrong direction

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -9,7 +9,6 @@ var CHESLIDESHOW = (function () {
 			if (running) {
 				return false;
 			}
-
 			running = true;
 
 			var nextSlide = slideSelector[currentSlide + 1] === undefined ?
@@ -19,6 +18,9 @@ var CHESLIDESHOW = (function () {
 
 			slideSelector[nextSlide].classList.add("left-position");
 			slideSelector[nextSlide].classList.remove("inactive");
+
+			indicatorSelector[currentSlide].classList.add("inactive-indicator");
+			indicatorSelector[nextSlide].classList.remove("inactive-indicator");
 			
 			setTimeout(function() {
 				slideSelector[nextSlide].classList.add("activating-from-left");
@@ -41,7 +43,6 @@ var CHESLIDESHOW = (function () {
 
 			//slideSelector[currentSlide].classList.add("inactive");
 			//slideSelector[nextSlide].classList.remove("inactive");
-		
 
 
 			//slideSelector[currentSlide].classList.add("inactive");
@@ -51,20 +52,42 @@ var CHESLIDESHOW = (function () {
 			//indicatorSelector[nextSlide].classList.remove("inactive-indicator");
 
 			//currentSlide = nextSlide;
-
 		},
 		prevSlide: function () {
+			if (running) {
+				return;
+			}
+			running = true;
 			
 			var prevSlide = slideSelector[currentSlide - 1] === undefined ?
 				slideSelector.length - 1 :  currentSlide - 1;
 
-			slideSelector[currentSlide].classList.add("inactive");
-			indicatorSelector[currentSlide].classList.add("inactive-indicator");
-		
-			slideSelector[prevSlide].classList.remove("inactive");
-			indicatorSelector[prevSlide].classList.remove("inactive-indicator");
+			slideSelector[currentSlide].classList.remove("trans-none");
 
-			currentSlide = prevSlide;
+			slideSelector[prevSlide].classList.add("left-position");
+			slideSelector[prevSlide].classList.remove("inactive");
+
+			indicatorSelector[currentSlide].classList.add("inactive-indicator");
+			indicatorSelector[prevSlide].classList.remove("inactive-indicator");
+			
+			setTimeout(function() {
+				slideSelector[prevSlide].classList.add("activating-from-left");
+				slideSelector[currentSlide].classList.add("deactivating-to-right");
+
+				setTimeout(function() {
+					slideSelector[currentSlide].classList.add("inactive");
+					slideSelector[currentSlide].classList.remove("deactivating-to-right");
+					slideSelector[prevSlide].classList.add("trans-none");
+					slideSelector[prevSlide].classList.remove("activating-from-left");
+					slideSelector[prevSlide].classList.remove("left-position");
+					setTimeout(function() {
+						running = false;
+						currentSlide = prevSlide;
+					}, 1);
+			    }, 1000);
+
+
+			}, 1);
 			
 		},
 		selectSlide: function (slide) {

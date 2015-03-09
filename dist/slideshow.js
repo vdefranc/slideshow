4026d6818a497b904ca5bd15808eb60daee3c195
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-03-09 14:57:47 -0400

    added comments

--- dist/slideshow.js
+++ dist/slideshow.js
@@ -1,28 +1,34 @@
 var CHESLIDESHOW = (function () {
-	var currentSlideIndex = 0,
+	var numSlides = 4,
+		currentSlideIndex = 0,
 		running = false,
 	 	slideSelector = document.getElementsByClassName('che-slideshow-slide'),
 		indicatorSelector = document.getElementsByClassName('slideshow-indicator'),
 		currentSlide,
-		mode,
-		newSlide;
+		newSlide,
+		mode;
 
 	function animateSlides (newMode) {
+		// prevents function from running while an animation is active
 		if (running) {
 			return false;
 		}
 		running = true;
 
+		//define target slides
 		setTargets(newMode);
 
+		// set indicators to next state
 		indicatorSelector[currentSlideIndex].classList.add('inactive-indicator');
 		indicatorSelector[newSlideIndex].classList.remove('inactive-indicator');
 
+		//set staging position
 		newSlide.style.left = mode === 'prev' ? '-100%' : '100%';
 		currentSlide.style.left = '0%';
 
 		newSlide.classList.remove("inactive");
 	
+		//begins both animations
 		animate(newSlide);
 		animate(currentSlide);
 	}
@@ -30,16 +36,19 @@ var CHESLIDESHOW = (function () {
 	function animate (slide) {
 		var i = 0;
 
+		// animation interval
 		var animationInt = setInterval(function(){
-			slide.style.left = mode === 'prev' ? ( parseInt(slide.style.left) + 1 ) + "%"
-												: ( parseInt(slide.style.left) - 1 ) + "%";
+			//animate one tick according to direction
+			slide.style.left = mode === 'prev' ? ( parseInt(slide.style.left) + 2 ) + "%"
+												: ( parseInt(slide.style.left) - 2 ) + "%";
 			i++;
 
-			if(i >= 100) {
+			if(i >= 50) {
 				stopAnimation();
 			}
-		},10);
+		}, 15);
 
+		// clears interval, returns all elements to a nominal state
 		function stopAnimation () {
 			currentSlide.classList.add('inactive');
 			newSlide.style.left = '0%';
@@ -49,13 +58,15 @@ var CHESLIDESHOW = (function () {
 		}
 	}
 
+	// defines target slides according to direction of animation 
 	function setTargets(newMode) {
 		mode = newMode;
 
+		// get the index of the next slide. hard reset to 0 or numSlides (-1) so slideshow can loop back
 		if (mode === "prev") {
-			newSlideIndex = slideSelector[currentSlideIndex - 1] === undefined ? 3 :  currentSlideIndex - 1;
+			newSlideIndex = slideSelector[currentSlideIndex - 1] === undefined ? (numSlides - 1) : currentSlideIndex - 1;
 		} else {
-			newSlideIndex = slideSelector[currentSlideIndex + 1] === undefined ? 0 :  currentSlideIndex + 1;
+			newSlideIndex = slideSelector[currentSlideIndex + 1] === undefined ? 0 : currentSlideIndex + 1;
 		}
 
 		currentSlide = slideSelector[currentSlideIndex];
@@ -65,11 +76,13 @@ var CHESLIDESHOW = (function () {
 	window.onload = setContainerSize;
 	window.onresize = setContainerSize;
 
+	// sets the container height to the height of slides. height not defined in css. 
 	function setContainerSize() {
 		var container = document.getElementsByClassName('che-slideshow')[0];
 		container.style.height = getComputedStyle(slideSelector[currentSlideIndex]).height;
 	}
 
+	// expose the private animate function & call with appropriate argument
 	return {
 		nextSlide: function () {
 			animateSlides('next');

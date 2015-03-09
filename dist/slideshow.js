d5820a83fed03b52d78e14e5630c2f4e21abb3e9
Vincent DeFrancesco <vincentg.defrancesco@gmail.com>
2015-03-09 14:04:47 -0400

    cleaned up script

--- dist/slideshow.js
+++ dist/slideshow.js
@@ -3,102 +3,74 @@ var CHESLIDESHOW = (function () {
 		running = false,
 	 	slideSelector = document.getElementsByClassName('che-slideshow-slide'),
 		indicatorSelector = document.getElementsByClassName('slideshow-indicator'),
+		prevOrNext,
 		currentSlide,
+		mode,
 		newSlide;
 
-	// var next = function () {};
-	// var prev = function () {};
-
-	var animateSlides = function (newSlide, currentSlide, prevOrNext, newSlideIndex) {
-		var notDirection = prevOrNext === 'prev' ? 'next' : 'prev';
-		
-		var animate = function(slideParameter) {
-			var slide = slideParameter;
-			var i = 0;
-
-
-			var animationInt = setInterval(function(){
-				if (prevOrNext === "prev") {
-					slide.style.left = ( parseInt(slide.style.left) + 1 ) + "%";
-				} else {
-					slide.style.left = ( parseInt(slide.style.left) - 1 ) + "%";
-				}
-				i++;
-
-				if(i >= 100) {
-					stopAnimation();
-				}
-			},10);
-
-			var stopAnimation = function () {
-				currentSlide.classList.add('inactive');
-				newSlide.style.left = '0%';
-				clearInterval(animationInt);
-				running = false;
-				//currentSlide = newSlide;
-				currentSlideIndex = newSlideIndex;
-				console.log("currentSlideIndex: " + currentSlideIndex);
-				console.log("currentSlide: " + currentSlide);
-				console.log("newSlide: " + newSlide);
-				
-			};
-
-		};
-
-		//RUNS BEFORE ABOVE FUNCTIONS inside animateSlides
-
-		if (prevOrNext === "prev") {
-			newSlide.style.left = '-100%';
-			currentSlide.style.left = '0%';
-		} else {
-			newSlide.style.left = '100%';
-			currentSlide.style.left = '0%';
+	function animateSlides (newMode) {
+		if (running) {
+			return false;
 		}
+		running = true;
+
+		setTargets(newMode);
+
+		indicatorSelector[currentSlideIndex].classList.add('inactive-indicator');
+		indicatorSelector[newSlideIndex].classList.remove('inactive-indicator');
+
+		newSlide.style.left = mode === 'prev' ? '-100%' : '100%';
+		currentSlide.style.left = '0%';
 
 		newSlide.classList.remove("inactive");
 	
 		animate(newSlide);
 		animate(currentSlide);
+	}
 
-	};
+	function animate (slide) {
+		var i = 0;
+
+		var animationInt = setInterval(function(){
+			slide.style.left = mode === 'prev' ? ( parseInt(slide.style.left) + 1 ) + "%"
+												: ( parseInt(slide.style.left) - 1 ) + "%";
+			i++;
+
+			if(i >= 100) {
+				stopAnimation();
+			}
+		},10);
+
+		function stopAnimation () {
+			currentSlide.classList.add('inactive');
+			newSlide.style.left = '0%';
+			clearInterval(animationInt);
+			currentSlideIndex = newSlideIndex;	
+			running = false;
+		}
+
+	}
+
+	function setTargets(newMode) {
+		mode = newMode;
+
+		if (mode === "prev") {
+			newSlideIndex = slideSelector[currentSlideIndex - 1] === undefined ? 3 :  currentSlideIndex - 1;
+			currentSlide = slideSelector[currentSlideIndex];
+			newSlide = slideSelector[newSlideIndex];
+		} else {
+			newSlideIndex = slideSelector[currentSlideIndex + 1] === undefined ? 0 :  currentSlideIndex + 1;
+			currentSlide = slideSelector[currentSlideIndex];
+			newSlide = slideSelector[newSlideIndex];
+		}
+	}
 
 	return {
 		nextSlide: function () {
-			if (running) {
-				return false;
-			}
-			running = true;
-
-			var newSlideIndex = slideSelector[currentSlideIndex + 1] === undefined ?
-				0 :  currentSlideIndex + 1,
-
-				currentSlide = slideSelector[currentSlideIndex],
-				newSlide = slideSelector[newSlideIndex];
-
-				indicatorSelector[currentSlideIndex].classList.add('inactive-indicator');
-				indicatorSelector[newSlideIndex].classList.remove('inactive-indicator');
-
-				animateSlides(newSlide, currentSlide, 'next', newSlideIndex);
+			animateSlides('next');
 		},
 		prevSlide: function () {
-			if (running) {
-				return false;
-			}
-			
-			running = true;
-
-
-
-			var newSlideIndex = slideSelector[currentSlideIndex - 1] === undefined ?
-				3 :  currentSlideIndex - 1,
-
-				currentSlide = slideSelector[currentSlideIndex],
-				newSlide = slideSelector[newSlideIndex];
-
-				indicatorSelector[currentSlideIndex].classList.add('inactive-indicator');
-				indicatorSelector[newSlideIndex].classList.remove('inactive-indicator');
-
-				animateSlides(newSlide, currentSlide, 'prev', newSlideIndex);
+			animateSlides('prev');
 		}
 	};
 })();
